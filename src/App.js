66081a3883e7c28da09ca3b1fc0fd26cfd53d336
Sairoden <odingandarosa@gmail.com>
2021-12-06 12:51:16 +0800

    Center element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.scss";
+import loader from "./loader.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>UNSPLASH API - INFINITE SCROLL</h1>
+      <div className="loader" id="loader">
+        <img src={loader} alt="Loading" hidden />
+      </div>
     </div>
   );
 }
